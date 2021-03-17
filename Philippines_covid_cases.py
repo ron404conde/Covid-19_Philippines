74958f36c302b1b3dc0dc3758726d1d0f0a7e6e1
ron404conde <sollezarondel@gmail.com>
2021-03-17 09:56:40 +0800

    Initial Commit

---
+++ Philippines_covid_cases.py
@@ -0,0 +1,18 @@
+import json
+from urllib.request import urlopen
+
+def get_covid_cases():
+    with urlopen("https://api.apify.com/v2/key-value-stores/lFItbkoNDXKeSWBBA/records/LATEST?disableRedirect=true") as response:
+        source = response.read()
+        data = json.loads(source)
+
+        print("Country: ", json.dumps(data['country']))
+        print("Infected: ", json.dumps(data['infected']))
+        print("Tested: ", json.dumps(data['tested']))
+        print("Recovered: ", json.dumps(data['recovered']))
+        print("Deaths: ", json.dumps(data['deceased']))
+        print("Active Cases: ", json.dumps(data['activeCases']))
+
+# main call
+if __name__ == "__main__":
+    get_covid_cases()
